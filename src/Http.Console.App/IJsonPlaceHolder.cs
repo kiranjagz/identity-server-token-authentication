86596907abbed97d13c8a5841115d04219358d71
Kiran Jagathlal <Kiran.Jagathlal@standardbank.co.za>
2024-02-15 16:00:15 +0200

    feat: added a little http client to help with DI

---
+++ src/Http.Console.App/IJsonPlaceHolder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Http.Console.App
+{
+    public interface IJsonPlaceHolder
+    {
+        Task<string> Get();
+    }
+
+    public class JsonPlaceHolder : IJsonPlaceHolder
+    {
+        private HttpClient _httpClient;
+
+        public JsonPlaceHolder(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> Get()
+        {
+            using HttpResponseMessage response = await _httpClient.GetAsync("todos/3");
+
+            response.EnsureSuccessStatusCode();
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            
+            return jsonResponse;
+        }
+    }
+}
---
+++ src/Http.Console.App/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Security.Authentication.ExtendedProtection;
+
+namespace Http.Console.App
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // register the services upfront
+            var services = new ServiceCollection();
+
+            services.AddHttpClient<IJsonPlaceHolder, JsonPlaceHolder>(client =>
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+            });
+
+            //services.AddScoped<IJsonPlaceHolder, JsonPlaceHolder>();
+
+            // build the servics
+            var serviceProvider = services.BuildServiceProvider();
+
+            // resolve the service
+            var myService = serviceProvider.GetRequiredService<IJsonPlaceHolder>();
+
+            // use service
+            var data = await myService.Get();
+
+            await System.Console.Out.WriteLineAsync(data);
+
+
+        }
+    }
+}
+
